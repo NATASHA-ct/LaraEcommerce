f8f767e7355b023758908b0a3f16b0b7f9c6699b
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-06-20 11:19:58 +0300

    Add my orders page

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -19,7 +19,7 @@ if(Session::has('user'))
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/myorders">Orders</a>
+          <a class="nav-link" href="/myorders">My-Orders</a>
         </li>
       </ul>
       <form  action="/search" class="d-lg-flex d-none mx-auto" role="search">
