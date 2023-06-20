28e35b1d1c7bc48e5a14a0b0d5123f259bd985ce
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-06-20 10:12:31 +0300

    Remove syntax error on the login method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     public function login(Request $req){
        $user = User::where(['email'=> $req->email])->first();
 
-        if($user || Hash::check($req->password, $user->$password)){
+        if($user || Hash::check($req->password, $user->password)){
            $req->session()->put('user', $user);
             return redirect('/'); 
         }
