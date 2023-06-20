c876e984c828b4ed19e1bd62e7c337d77936ddbd
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-06-20 10:16:46 +0300

    Implent remove item from cart, modify nav

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -60,5 +60,12 @@ class ProductController extends Controller
 
         return view('cartlist',['products'=>$products]);
     }
-    
+
+     function removeCart($id)
+    {
+        Cart::destroy($id);
+        return redirect('cartlist');
+    }
+ 
+
 }
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -9,20 +9,20 @@ if(Session::has('user'))
 <header class="mb-5">
    <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand text-primary" href="#">LaraEshop</a>
     <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Orders</a>
+          <a class="nav-link" href="/myorders">Orders</a>
         </li>
       </ul>
-      <form class="d-lg-flex d-none mx-auto" role="search">
+      <form  action="/search" class="d-lg-flex d-none mx-auto" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
