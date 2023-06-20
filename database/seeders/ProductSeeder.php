482e8f91596ae788319181e06698eafa858b22b5
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-06-20 10:11:48 +0300

    Add seeders to DB seeder and add more products

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -44,7 +44,22 @@ class ProductSeeder extends Seeder
                 "description"=>"A fridge with much more feature",
                 "category"=>"fridge",
                 "gallery"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFx-2-wTOcfr5at01ojZWduXEm5cZ-sRYPJA&usqp=CAU"
-             ]
+            ],
+            [
+                'name'=>'Panasonic Tv',
+                "price"=>"400",
+                "description"=>"A smart tv with much more feature",
+                "category"=>"tv",
+                "gallery"=>"https://i.gadgets360cdn.com/products/televisions/large/1548154685_832_panasonic_32-inch-lcd-full-hd-tv-th-l32u20.jpg"
+            ],
+            [
+                'name'=>'Oppo mobile',
+                "price"=>"300",
+                "description"=>"A smartphone with 8gb ram and much more feature",
+                "category"=>"mobile",
+                "gallery"=>"https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBOPPO-A52-6-GFUTU6297453D3D253C/1592019058170_0..png"
+            ],
+
         ]);
     }
 }
